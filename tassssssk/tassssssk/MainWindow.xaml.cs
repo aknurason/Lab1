d495fde0fdfa9e61a928cd1b81f70cd3c40e5580
Yelamanova Aknur <aknurason@gmail.com>
2019-02-13 01:16:38 +0600

    as

---
+++ tassssssk/tassssssk/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Task4
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[,] arr = new string[n, n];
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                for (int j = 0; j < n; j++)
+
+                {
+
+                    if (i >= j) arr[i, j] = "[*]";
+
+                }
+
+            }
+
+
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                for (int j = 0; j < n; j++)
+
+                {
+
+                    Console.Write(arr[i, j]);
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+}
